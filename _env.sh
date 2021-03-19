95f0434bbf15c4fff6b51cd595a49ba91c83e10b
Jean Guillaume <jgi@jgwill.com>
2021-03-19 05:45:30 -0400

    work

---
+++ _env.sh
@@ -0,0 +1,28 @@
+
+
+
+# ARTIST : CHG Model name
+
+modelname="model_gia-ds-wassily_kandinsky_v1_210310_new-210ik"
+
+
+
+# ARTIST : Probably dont change anything bellow 
+
+## Environment path related
+modelmountpath="/mnt/c/model/models"
+
+
+## Container related
+containermodelroot="/data/styleCheckpoint"
+
+
+# Infrastructure related
+containertag="guillaumeai/ast:runwayml_picasso_2103180139"
+
+serverport=8000
+serverhostport=9000
+docker_cmd="docker run -it --rm "
+run_cmd="bash"
+
+
--- custom-start-docker-ast-picajgwill-v02.sh
+++ custom-start-docker-ast-picajgwill-v02.sh
@@ -1,6 +1,25 @@
 
-
 source _env.sh
+
+# ARTIST : CHG Model name
+
+ ## Override of _env for custem
+
+modelname="model_gia-ds-wassily_kandinsky_v1_210310_new-210ik"
+modelname="model_gia-young-picasso-v02-201210-240ik"
+
+
+
+# ARTIST : Probably dont change anything bellow 
+
+
+serverport=8000
+serverhostport=9002
+docker_cmd="docker run -it --rm "
+run_cmd="bash"
+
+
+
 
 
 modelmountpoint="$containermodelroot/$modelname/checkpoint_long"
