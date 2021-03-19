4e07fb56f1ee03f89057d9f4b4bef631d2a1ac55
Jean Guillaume <jgi@jgwill.com>
2021-03-19 05:50:16 -0400

    feat:hostenvfile

--- _env.sh
+++ _env.sh
@@ -3,26 +3,33 @@
 
 # ARTIST : CHG Model name
 
-modelname="model_gia-ds-wassily_kandinsky_v1_210310_new-210ik"
+export modelname="model_gia-ds-wassily_kandinsky_v1_210310_new-210ik"
 
 
 
 # ARTIST : Probably dont change anything bellow 
 
 ## Environment path related
-modelmountpath="/mnt/c/model/models"
+export modelmountpath="/mnt/c/model/models"
 
 
 ## Container related
-containermodelroot="/data/styleCheckpoint"
+export containermodelroot="/data/styleCheckpoint"
 
 
 # Infrastructure related
-containertag="guillaumeai/ast:runwayml_picasso_2103180139"
+export containertag="guillaumeai/ast:runwayml_picasso_2103180139"
 
-serverport=8000
-serverhostport=9000
-docker_cmd="docker run -it --rm "
-run_cmd="bash"
+export serverport=8000
+export serverhostport=9000
+export docker_cmd="docker run -it --rm "
+export run_cmd="bash"
 
 
+# Loads an ENV for the current host if exist
+hostenvfile="_henv_$HOSTNAME.sh"
+if [ -f $hostenvfile ]; then
+    . ./$hostenvfile
+else
+    echo " ./$hostenvfile does not exist, create it if you require to define specific to platform variable or overwrite some."
+fi
