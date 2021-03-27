730638e1ce6f24105e6c21afa5b21f902eef651d
JGWill-Orko <jgi@jgwill.com>
2021-03-27 06:57:16 -0400

    feat:dali

---
+++ custom-start-docker-ast-daliwill-180ik.sh
@@ -0,0 +1,17 @@
+
+
+source _env.sh
+
+# ARTIST : CHG Model name
+
+
+export modelname="model_gia-ds-daliwill-210123-v01_new-180ik"
+
+
+
+
+export serverhostport=9004
+
+
+source __launch-docker.sh
+
