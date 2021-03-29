5a962a63883448019ac64da5a91ba751d9676452
JGWill-Orko <jgi@jgwill.com>
2021-03-29 12:15:22 -0400

    feat:new-models

---
+++ custom-start-docker-ast-picajgwill-v04-120ik.sh
@@ -0,0 +1,19 @@
+
+
+source _env.sh
+
+# ARTIST : CHG Model name
+
+
+
+
+export modelname="model_giapicallo_v04__201221-120ik"
+
+export containername=ast_picasso_var4_120
+
+
+export serverhostport=9008
+
+
+source __launch-docker.sh
+
