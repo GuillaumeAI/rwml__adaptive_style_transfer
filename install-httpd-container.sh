24692bb3a28fcd32eb2c47c7df0711e528dfaaa7
Jean Guillaume <jgi@jgwill.com>
2021-04-01 14:56:51 -0400

    doc

--- install-httpd-container.sh
+++ install-httpd-container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# Will be moved to all host - keep it here to stay compatible with new host and ol0d ones
+export httpdserverhtdocs=/a/src/x__etch-a-sketch__210224
 source _env.sh
 
 # Exports...
@@ -9,12 +10,21 @@ source _env.sh
 export httpdserverport=80
 export httpdcontainerport=80
 export httpdcontainerhtdocs=/usr/local/apache2/htdocs
-export httpdserverhtdocs=/a/src/x__etch-a-sketch__210224
+
+#export httpdserverhtdocs=/a/src/x__etch-a-sketch__210224 # Moving it to the host env
 export httpdcontainertag="guillaumeai/server:httpd"
 export httpcontainername=ast_webapp
 
 #echo docker run -d $docker_run_args --name $httpcontainername -p $httpdserverport:$httpdcontainerport -v $(pwd):/work -v $httpdserverhtdocs:$httpdcontainerhtdocs -v /a:/a -v /a:$httpdcontainerhtdocs/a $httpdcontainertag
-docker rm $$httpcontainername
+echo "--- Cleaning up if existing container----"
+docker rm $httpcontainername
+echo "--- Cleanup done (if was required, otherwise ignore the errors-------"
+echo "---------------------------------------------------------------------"
+
+echo "-------- Installing and running the container : $httpdcontainertag --"
+echo "-- Host HTTPDocPath: $httpdserverhtdocs          -"
+echo "-- Being served at port : $httpdserverport:$httpdcontainerport     -"
+echo "--------------------------------------------------------------------"
 
 docker run -d $docker_run_args --name $httpcontainername -p $httpdserverport:$httpdcontainerport -v $(pwd):/work -v $httpdserverhtdocs:$httpdcontainerhtdocs -v /a:/a -v /a:$httpdcontainerhtdocs/a $httpdcontainertag
 
