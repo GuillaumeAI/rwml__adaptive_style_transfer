2cef01c11b8a56e42c830b7952623d9b50a45611
Jean Guillaume <jgi@jgwill.com>
2021-04-01 14:40:41 -0400

    add:env

---
+++ _henv_DESKTOP-07KN68A.sh
@@ -0,0 +1,16 @@
+# Env def for host
+# local Mount
+## Environment path related
+export modelmountpath="/a/model/models"
+export hostdns="orko.guillaumeisabelle.com"
+#export callprotocol="http"
+#export callurl="$callprotocol://$hostdns:$serverhostport"
+
+
+# docker
+#export docker_cmd="docker run -it --rm "
+## Set to desired Docker running Mode (it:iteractive, d:background)
+#export docker_mode="it"
+export docker_mode="d"
+
+
