4c77e01373af3012bf6a2b01a5da8b756416058c
JGWill-Orko <jgi@jgwill.com>
2021-04-01 20:36:45 -0400

    feat:monet-vd

---
+++ custom-start-docker-ast-monet-vd.sh
@@ -0,0 +1,20 @@
+
+
+source _env.sh
+
+# ARTIST : CHG Model name
+
+
+
+
+export modelname="model_monet"
+
+
+export containername=ast_monet_vd
+
+
+export serverhostport=9014
+
+
+source __launch-docker.sh
+
