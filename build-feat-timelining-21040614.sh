2d502893cb7c418eef73c110460f1e747e697d81
JGWill-Orko <jgi@jgwill.com>
2021-04-06 14:21:25 -0400

    refacto

--- build-feat-timelining-21040614.sh
+++ build-feat-timelining-21040614.sh
@@ -1,4 +1,15 @@
 source _env.sh
 featuretag=feat-timelining-21040614
-docker build -t $containertag-$featuretag dockers/$featuretag
+featurebuildscript=dockers/$featuretag
 
+echo "Building : $containertag"
+echo " --Feature: $featuretag"
+echo " ------ From:  $featurebuildscript"
+echo "--------------------------------"
+docker build -t $containertag-$featuretag $featurebuildscript
+
+echo "DONE Building : $containertag"
+echo " --Feature: $featuretag"
+
+echo " ------ From:  $featurebuildscript"
+echo "--------------------------------"
