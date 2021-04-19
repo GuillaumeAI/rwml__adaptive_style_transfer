6f54b163adb883a62c28e0d60e7240b23d8d0abf
JGWill <jgi@jgwill.com>
2021-04-19 18:15:13 -0400

    feat:Compo2104 models

---
+++ custom-start-docker-ast-picajgwill-v03-120ik.sh
@@ -0,0 +1,18 @@
+
+
+source _env.sh
+
+# ARTIST : CHG Model name
+
+
+
+export modelname="model_gia-young-picasso-v03-201216_new-120ik"
+
+export containername=ast_picasso_var3_120
+
+
+export serverhostport=9031
+
+
+source __launch-docker.sh
+
---
+++ custom-start-docker-ast-picajgwill-v05-150ik.sh
@@ -0,0 +1,18 @@
+
+
+source _env.sh
+
+# ARTIST : CHG Model name
+
+
+
+export modelname="model_gia-young-picasso-v05-2101082232_new-150ik"
+
+export containername=ast_picasso_var5_150
+
+
+export serverhostport=9031
+
+
+source __launch-docker.sh
+
