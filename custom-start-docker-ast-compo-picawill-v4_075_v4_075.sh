f2f96c28c8c4b65f8a49cbd7b3beae0d7aab3427
JGWill <jgi@jgwill.com>
2021-04-20 11:43:55 -0400

    feat(compo):Trying to add a compo server

---
+++ custom-start-docker-ast-compo-picawill-v4_075_v4_075.sh
@@ -0,0 +1,23 @@
+source _env.sh
+
+# ARTIST : CHG Model name
+
+export modelname="model_gia-young-picasso-v04-2012211928_new-75ik"
+export model2name="model_gia-young-picasso-v04-2012211928_new-75ik"
+# The resolution of the two passes
+export PASS1IMAGESIZE=300
+export PASS2IMAGESIZE=2048
+
+export containername=compo_ast_picawill_v4_075_v4_075
+
+
+export serverhostport=9052
+
+#export docker_mode="it"
+export docker_mode="d"
+#export docker_run_args="--rm "
+export docker_run_args="--restart unless-stopped "
+
+
+
+source __launch-docker-compo.sh
