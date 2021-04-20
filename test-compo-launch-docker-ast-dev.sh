a5f21116ab73ab0a0d5e2c5c332e0eb2b4b98263
JGWill-Orko <jgi@jgwill.com>
2021-04-20 10:10:24 -0400

    fix:name

--- test-compo-launch-docker-ast-dev.sh
+++ test-compo-launch-docker-ast-dev.sh
@@ -8,7 +8,7 @@ export model2name="model_gia-young-picasso-v03-201216_new-120ik"
 export PASS1IMAGESIZE=1024
 export PASS2IMAGESIZE=2048
 
-export containername=compo_ast_picawill_v5_150_v3_120
+export containername=test_compo_ast_picawill_v5_150_v3_120
 
 
 export serverhostport=9090
