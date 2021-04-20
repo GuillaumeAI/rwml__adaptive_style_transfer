dea65bdfb62e3f69aaeb34689b036b1d136caf20
JGWill-Orko <jgi@jgwill.com>
2021-04-20 12:43:42 -0400

    feat

--- custom-start-docker-ast-compo-picawill-v4_195_v3_120.sh
+++ custom-start-docker-ast-compo-picawill-v4_195_v3_120.sh
@@ -3,15 +3,15 @@ source _env.sh
 # ARTIST : CHG Model name
 
 export modelname="model_gia-young-picasso-v04-2012211928_new-195ik"
-export model2name="model_gia-young-picasso-v03-201216_new-180ik"
+export model2name="model_gia-young-picasso-v03-201216_new-120ik"
 # The resolution of the two passes
 export PASS1IMAGESIZE=512
 export PASS2IMAGESIZE=1920
 
-export containername=compo_ast_picawill_v4_195_v3_180
+export containername=compo_ast_picawill_v4_195_v3_120
 
 
-export serverhostport=9055
+export serverhostport=9056
 
 #export docker_mode="it"
 export docker_mode="d"
