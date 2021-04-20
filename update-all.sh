772ab2a5bed3120838ee14958a2b7e99707cfb70
JGWill <jgi@jgwill.com>
2021-04-20 09:09:21 -0400

    add

---
+++ update-all.sh
@@ -0,0 +1,22 @@
+
+
+source _env.sh
+# Pulling docker images
+docker pull $containertag
+docker pull $compocontainertag
+
+#@TODO More update
+
+#for s in custom-start-docker-ast-*.sh ; do
+#	echo "Launching/Installing : $s"
+#	./$s
+#done
+
+#echo "Launching/Installing WebApp..."
+#./install-httpd-container.sh
+
+#./custom-start-docker-ast-kandinsky-v01.sh
+#./custom-start-docker-ast-picajgwill-v02.sh
+#./custom-start-docker-ast-picajgwill-v03-030ik.sh
+#./custom-start-docker-ast-picajgwill-v03-165ik.sh
+
