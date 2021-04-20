e146e5ea87a6c0297d8244da27bd782f7b3491c5
JGWill <jgi@jgwill.com>
2021-04-20 10:09:43 -0400

    feat(compo):Background install on port 9050

---
+++ __launch-docker-compo.sh
@@ -0,0 +1,34 @@
+
+
+# Ran by another script that preped all we need
+
+export docker_cmd="docker run -$docker_mode $docker_run_args --name $containername "
+
+if [ $docker_mode = "d" ] ; then echo "Background infrastructure mode activated (will run in background until stopped or server rebooted)" ; fi
+if [ $docker_mode = "it" ] ; then echo "Foreground infrastructure mode activated (require to keep the startup shell active)" ; fi
+
+
+export callurl="$callprotocol://$hostdns:$serverhostport"
+
+export modelmountpoint="$containermodelroot/$modelname/checkpoint_long"
+export model2mountpoint="$containermodelroot/$model2name/checkpoint_long"
+export modellocalpoint="$modelmountpath/$modelname/checkpoint_long"
+export model2localpoint="$modelmountpath/$model2name/checkpoint_long"
+
+echo "-------------------------------------------------------------"
+echo "- Running  Guillaume's M.A. AI AST Compo Model Server      -"
+echo "- Model Name: $modelname"
+echo "- Model2 Name: $model2name"
+echo "- AccessURL : $callurl"
+echo "-------------------------------------------------------------"
+#$docker_cmd -v $(pwd):/work -p 8000:9000 -v $modellocalpoint:$modelmountpoint -p $serverhostport:$serverport $containertag $run_cmd
+echo "----------Cleaning up-------"
+docker stop $containername
+docker rm $containername
+echo "-----------Installing $containername ------------"
+
+#echo $docker_cmd -v $(pwd):/work  -v $model2localpoint:$model2mountpoint  -v $modellocalpoint:$modelmountpoint -p $serverhostport:$serverport -e SPORT=$serverhostport $containertag
+#sleep 1
+#$docker_cmd -v $(pwd):/work  -v $modellocalpoint:$modelmountpoint -p $serverhostport:$serverport -e FOO=blabla -e SPORT=$serverhostport $containertag
+$docker_cmd -v $(pwd):/work  -v $model2localpoint:$model2mountpoint  -v $modellocalpoint:$modelmountpoint -p $serverhostport:$serverport  -e PASS1IMAGESIZE=$PASS1IMAGESIZE -e PASS2IMAGESIZE=$PASS2IMAGESIZE -e SPORT=$serverhostport $compocontainertag
+
---
+++ custom-start-docker-ast-compo-picawill-v5_150_v3_120.sh
@@ -0,0 +1,24 @@
+source _env.sh
+
+# ARTIST : CHG Model name
+
+export modelname="model_gia-young-picasso-v05-2101082232_new-150ik"
+export model2name="model_gia-young-picasso-v03-201216_new-120ik"
+# The resolution of the two passes
+export PASS1IMAGESIZE=1024
+export PASS2IMAGESIZE=2048
+
+export containername=compo_ast_picawill_v5_150_v3_120
+
+
+export serverhostport=9050
+
+#export docker_mode="it"
+export docker_mode="d"
+#export docker_run_args="--rm "
+export docker_run_args="--restart unless-stopped "
+
+
+
+#source __launch-docker.sh
+source __launch-docker-test-compo.sh
