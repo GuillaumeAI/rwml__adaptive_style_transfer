21a1407aeadfc9d82b7eddabaea10954ebab29e1
JGWill-Orko <jgi@jgwill.com>
2021-05-02 10:53:45 -0400

    mode

--- dockers/guillaumeai__server__ast-210502-compo-three/build.sh
+++ dockers/guillaumeai__server__ast-210502-compo-three/build.sh
@@ -1,7 +1,7 @@
 containertag=guillaumeai/server:ast-210502-compo-three
 
-#docker build -t $containertag . --no-cache
-docker build -t $containertag . 
+docker build -t $containertag . --no-cache
+#docker build -t $containertag . 
 
 docker push $containertag
 
