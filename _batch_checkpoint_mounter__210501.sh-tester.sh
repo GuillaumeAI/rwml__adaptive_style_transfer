4cc9ae491dbd14507d93b7d4218d61adb1d5e9b2
JGWill <jgi@jgwill.com>
2021-05-02 06:13:08 -0400

    update

--- _batch_checkpoint_mounter__210501.sh-tester.sh
+++ _batch_checkpoint_mounter__210501.sh-tester.sh
@@ -5,17 +5,23 @@ if [ "$1" == "" ]; then
 	echo "Must specify a file"
 	exit 1
 fi
+cdir=$(pwd)
+wdir=tests/chks-210501
 
-mkdir -p tests
-cd tests
+cfile=$1
+cfileb=${cfile%.*}
+csfile='../_'$cfileb'.csm.jpg'
+wdir+='-'$cfileb
+
+mkdir -p $wdir
+cp -f $1 $wdir
+cd $wdir
 
 #s=./custom-cli-start-script-docker-dev-specific-checkpoint.210430.sh 
 
 m=model_gia-ds-fpolsonwill_v02_210424_new
 export chks_long_all="15 30 45 60 75 90 105 120 135 150 165 180 195 210 225 240 255 270 285 300"
 export partial_210108="60 75 90 105 120 135 150 165 180 195 210 225 240 255 270 285 300"
-
-
 
 export chks=$chks_long_all
 export chks=$partial_210108
@@ -25,6 +31,7 @@ export chks=$partial_210108
 #$s model_gia-ds-fpolsonwill_v02_210424_new 9093 240
 
 startport=9070
+startport=70
 for c in $chks
 do
 	echo "------------------------------------------------------------"
@@ -32,12 +39,23 @@ do
 	echo "- Model     : $m  "
 	echo "- checkpoint: $c "
 	echo "- on port   : $startport"
-	echo "gia-ast $1 $startport"
+	gia-ast $1 $startport
 
 	startport=$( expr $startport + 1 )
 done
+echo "--------DONE Infering----------------------------------"
 
-echo "file should have been genenated in ./tests"
-cd ..
+# Generate Contactt sheets for the checkpoints
+
+echo "-------------------------------------------------------"
+echo "Generating a Contact sheet with each checkpoint results"
+ 
+echo "-- Contact sheet file : $csfile"
+#gis-csm $csfile
+gis-csm
+
+
+echo "file should have been genenated in $wdir"
+cd $cdir
 
 
--- cd-ast-cli-test.sh
+++ cd-ast-cli-test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+
 cd $srcpath/x__etch-a-sketch__210224/cli-proto-01/tests
