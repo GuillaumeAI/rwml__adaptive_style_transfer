9c41f089f1e8968e43e56201ca854525004a668f
JGWill-Orko <jgi@jgwill.com>
2021-05-02 11:05:38 -0400

    add:added the third model, should work

--- compo-server-three.py
+++ compo-server-three.py
@@ -26,12 +26,19 @@ else:
    pass1_image_size = os.getenv('PASS1IMAGESIZE')
    print("PASS1IMAGESIZE value:" + pass1_image_size)
 
-pass2_image_size = 2048
+pass2_image_size = 1328
 if not os.getenv('PASS2IMAGESIZE'):
-   print("PASS12MAGESIZE env var non existent;using default:" + pass2_image_size)
+   print("PASS2IMAGESIZE env var non existent;using default:" + pass2_image_size)
 else:
    pass2_image_size = os.getenv('PASS2IMAGESIZE')
    print("PASS2IMAGESIZE value:" + pass2_image_size)
+
+pass3_image_size = 2048
+if not os.getenv('PASS3IMAGESIZE'):
+   print("PASS3IMAGESIZE env var non existent;using default:" + pass3_image_size)
+else:
+   pass3_image_size = os.getenv('PASS3IMAGESIZE')
+   print("PASS3IMAGESIZE value:" + pass3_image_size)
 
 
 
@@ -39,10 +46,13 @@ else:
 def setup(opts):
     sess = tf.Session()
     sess2 = tf.Session()
+    sess3 = tf.Session()
     init_op = tf.global_variables_initializer()
     init_op2 = tf.global_variables_initializer()
+    init_op3 = tf.global_variables_initializer()
     sess.run(init_op)
     sess2.run(init_op2)
+    sess3.run(init_op3)
     with tf.name_scope('placeholder'):
         input_photo = tf.placeholder(dtype=tf.float32,
                                      shape=[1, None, None, 3],
@@ -55,6 +65,7 @@ def setup(opts):
                            reuse=False)
     saver = tf.train.Saver()
     saver2 = tf.train.Saver()
+    saver3 = tf.train.Saver()
     path = opts['styleCheckpoint']
     #Getting the model name
     model_name = [p for p in os.listdir(path) if os.path.isdir(os.path.join(path, p))][0]    
@@ -65,30 +76,45 @@ def setup(opts):
 
     #Getting the model2 name
     model2_name = [p for p in os.listdir(path) if os.path.isdir(os.path.join(path, p))][1]
-    if not os.getenv('MODELNAME'):
-        dtprint("CONFIG::MODELNAME env var non existent;using default:" + model2_name)
+    if not os.getenv('MODEL2NAME'):
+        dtprint("CONFIG::MODEL2NAME env var non existent;using default:" + model2_name)
     else:
         model2_name = os.getenv('MODEL2NAME')
         
+    #Getting the model3 name
+    model3_name = [p for p in os.listdir(path) if os.path.isdir(os.path.join(path, p))][2]
+    if not os.getenv('MODEL3NAME'):
+        dtprint("CONFIG::MODEL3NAME env var non existent;using default:" + model3_name)
+    else:
+        model3_name = os.getenv('MODEL3NAME')
+        
     checkpoint_dir = os.path.join(path, model_name, 'checkpoint_long')
     checkpoint2_dir = os.path.join(path, model2_name, 'checkpoint_long')
+    checkpoint3_dir = os.path.join(path, model3_name, 'checkpoint_long')
     print("-----------------------------------------")
     print("modelname is : " + model_name)
     print("model2name is : " + model2_name)
+    print("model3name is : " + model3_name)
     print("checkpoint_dir is : " + checkpoint_dir)
     print("checkpoint2_dir is : " + checkpoint2_dir)
+    print("checkpoint3_dir is : " + checkpoint3_dir)
     print("-----------------------------------------")
     ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
     ckpt2 = tf.train.get_checkpoint_state(checkpoint2_dir)
+    ckpt3 = tf.train.get_checkpoint_state(checkpoint3_dir)
     ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
     ckpt2_name = os.path.basename(ckpt2.model_checkpoint_path)
+    ckpt3_name = os.path.basename(ckpt3.model_checkpoint_path)
     saver.restore(sess, os.path.join(checkpoint_dir, ckpt_name))
     saver2.restore(sess2, os.path.join(checkpoint2_dir, ckpt2_name))
+    saver3.restore(sess3, os.path.join(checkpoint2_dir, ckpt3_name))
     m1 = dict(sess=sess, input_photo=input_photo, output_photo=output_photo)
     m2 = dict(sess=sess2, input_photo=input_photo, output_photo=output_photo)
+    m3 = dict(sess=sess3, input_photo=input_photo, output_photo=output_photo)
     models = type('', (), {})()
     models.m1 = m1
     models.m2 = m2
+    models.m3 = m3
     return models
 
 
@@ -98,12 +124,14 @@ def stylize(models, inp):
     dtprint("Composing...")
     model = models.m1
     model2 = models.m2
+    model3 = models.m3
 
     #
     img = inp['contentImage']
     img = np.array(img)
     img = img / 127.5 - 1.
-    #@a Pass 1 RESIZE to 1024px the smaller side
+
+    #@a Pass 1 RESIZE to 1368px the smaller side
     image_size=pass1_image_size
     img_shape = img.shape[:2]
     alpha = float(image_size) / float(min(img_shape))
@@ -122,7 +150,7 @@ def stylize(models, inp):
     img = img[0]
     dtprint("INFO:Upresing Pass1 for Pass 2 (STARTING) ")
 
-    #@a Pass 2 RESIZE to 2048px the smaller side
+    #@a Pass 2 RESIZE to 1024px the smaller side
     image_size=pass2_image_size
     img_shape = img.shape[:2]
     
@@ -144,6 +172,32 @@ def stylize(models, inp):
     img = (img + 1.) * 127.5
     img = img.astype('uint8')
     img = img[0]
+
+    #pass3
+
+    #@a Pass 3 RESIZE to 2048px the smaller side
+    image_size=pass3_image_size
+    img_shape = img.shape[:2]
+    
+    
+    alpha = float(image_size) / float(min(img_shape))
+    dtprint ("DEBUG::pass2.imgshape:" +   str(tuple(img_shape)) + ", alpha:" + str(alpha))
+
+    img = scipy.misc.imresize(img, size=alpha)
+    dtprint("INFO:Upresing Pass2 (DONE) ")
+
+    #Iteration 3
+    img = np.array(img)
+    img = img / 127.5 - 1.
+    img = np.expand_dims(img, axis=0)
+    #@a INFERENCE PASS 3
+    dtprint("INFO:Pass3 inference (STARTING)")
+    img = model3['sess'].run(model3['output_photo'], feed_dict={model3['input_photo']: img})
+    dtprint("INFO:Pass3 inference (DONE)")
+    img = (img + 1.) * 127.5
+    img = img.astype('uint8')
+    img = img[0]
+    #pass3
 
     dtprint("INFO:Composing done")
     stop = time.time()
