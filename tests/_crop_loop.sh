bf7c0cf3beccacb6264c1d20b3c17b03edbf36f2
JGWill <jgi@jgwill.com>
2021-05-04 05:52:29 -0400

    hirez

--- tests/_crop_loop.sh
+++ tests/_crop_loop.sh
@@ -1,10 +1,12 @@
-
-
+x1=1368
+x2=1024
+x3=2048
+p=98
 s=./test-compo-three-v2-dev-args.sh
-for i in {19..20}
+for i in {5..99}
 do
 	echo "-----------$i------------"
 	source _crop.sh $1 $2 $i
-	$s 98 640 512 1024 $i
+	$s $p $x1 $x2 $x3 $i
 done	
 #convert $1 -set page -%[fx:w*0.1]-%[fx:h*0.1] -crop $3%x+0+0 $2
