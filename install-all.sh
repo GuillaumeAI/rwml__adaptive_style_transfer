824ac7adf3650aeda5055827b6e0ad51280dc246
JGWill-Orko <jgi@jgwill.com>
2021-05-07 12:05:01 -0400

    chg:disabled-httpd-using-lamp

--- install-all.sh
+++ install-all.sh
@@ -5,13 +5,21 @@ source _env.sh
 docker pull $containertag
 docker pull $compocontainertag
 
-for s in custom-start-docker-ast-*.sh ; do
-	echo "Launching/Installing : $s"
-	./$s
-done
+if [ "$1" != "--skip" ];then
+	for s in custom-start-docker-ast-*.sh ; do
+		echo "Launching/Installing : $s"
+		./$s
+	done
+fi
 
-echo "Launching/Installing WebApp..."
-./install-httpd-container.sh
+if [ "$1" == "--httpd" ] || [ "$2" == "--httpd" ]; then
+	echo "Launching/Installing WebApp..."
+	./install-httpd-container.sh
+else 
+	echo "HTTP Was skipped"
+	echo "use $0 --skip --httpd"
+	echo "  to start it"
+fi
 
 #./custom-start-docker-ast-kandinsky-v01.sh
 #./custom-start-docker-ast-picajgwill-v02.sh
