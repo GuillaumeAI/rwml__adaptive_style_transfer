ef9300a751250f3c6f7db88627e6182c37f81e77
JGWill-Orko <jgi@jgwill.com>
2021-05-16 03:55:31 -0400

    add:result-serve

--- tests/_droxUploader.sh
+++ tests/_droxUploader.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+dp=/Downloads__2101/ast210505/seq_jgi__SC__210411_04
+droxul mkdir $dp
 
 #loop upload to dropbox
-for i in {1..222};do for f in *jpg ; do droxul upload $f $dp;done ;sleep1233;done
+for i in {1..22};do for f in *jpg ; do sleep 333; droxul upload $f $dp;done ;sleep 1233;done
 
