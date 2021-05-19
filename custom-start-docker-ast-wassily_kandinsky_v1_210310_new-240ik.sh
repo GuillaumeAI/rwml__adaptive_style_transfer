d66ec0aecedf7492af7b14124e5d66694c76e2f3
JGWill-Orko <jgi@jgwill.com>
2021-05-18 22:51:16 -0400

    add

---
+++ custom-start-docker-ast-wassily_kandinsky_v1_210310_new-240ik.sh
@@ -0,0 +1,17 @@
+
+
+source _env.sh
+
+# ARTIST : CHG Model name
+
+
+export modelname="model_gia-ds-wassily_kandinsky_v1_210310_new-240ik"
+
+export containername=ast_kandinsky_v1_210310_240ik
+
+
+export serverhostport=9033
+
+
+source __launch-docker.sh
+
