7a7251b72ffa976c2cb36843dc23d149c469d54e
JGWill-Orko <jgi@jgwill.com>
2021-05-31 23:02:23 -0400

    integrating

--- synopsis__addDoubleTwoByCheckpoint.sh
+++ synopsis__addDoubleTwoByCheckpoint.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 export checkpointno=90
-export port=9070
+
+export serverhostport=9070
 export modelname=model_gia-ds-pierret_ds_210512-864-v02-210527-864_new
 export PASS1IMAGESIZE=1200
 export PASS2IMAGESIZE=1700
 
-./addDoubleTwoChk.sh \
-    $port \
-    $modelname \
-    $PASS1IMAGESIZE $PASS2IMAGESIZE 
+
+# ./addDoubleTwoChk.sh \
+#     $port \
+#     $modelname \
+#     $PASS1IMAGESIZE $PASS2IMAGESIZE 
+
+
+############################## BECAUSE WE ARE DEVELOPPING
+export scriptfn=custom-cli-start-script-docker-dev-specific-checkpoint.210430.sh
+#/a/src/rwml__adaptive_style_transfer/__launch-docker-dev-specific-checkpoint-doubletwo.210531.sh
+
+
+source $scriptfn "$modelname" $serverhostport $checkpointno $PASS1IMAGESIZE $PASS2IMAGESIZE 
+
