134d0b0898bcd30acd3dda73307402070f1287f9
JGWill-Orko <jgi@jgwill.com>
2021-06-10 08:52:27 -0400

    update

---
+++ custom-start-docker-ast-compo-dbg-v01b_345-singleone-dev-v1.sh
@@ -0,0 +1,32 @@
+source _env.sh
+
+# ARTIST : CHG Model name
+
+
+export modelname="model_gia-ds-DavidBouchardGagnon-v01b-210510-864_new"
+
+# The resolution of the two passes
+
+export PASS1IMAGESIZE=2100
+export checkpointno=345
+export containername=compo_ast_dbg_v01b_s1_$checkpointno
+
+
+export serverhostport=9038
+
+
+export docker_mode="d"
+
+export docker_run_args="--restart unless-stopped "
+
+
+source custom-cli-start-script-docker-dev-specific-checkpoint-singleone.210606.sh \
+	$modelname \
+	$serverhostport \
+	$PASS1IMAGESIZE \
+       	$checkpointno
+
+## MODELNAME APIPORT PASS1IMAGESIZE CHECKPOINT_NUM [--fg (debug)] "
+#source __launch-docker-compo-doubletwo-v1-dev.sh $1 $2
+
+
