ef7b88dbdda71c0d7b4105c728048ead0159667c
Ubuntu <ubuntu@ip-172-31-39-169.ca-central-1.compute.internal>
2021-06-18 01:57:23 +0000

    fix

--- osiris-launch-210618.sh
+++ osiris-launch-210618.sh
@@ -1,1 +1,1 @@
-custom-cli-start-script-docker-dev-specific-checkpoint-doubletwo.210531.sh model_gia-ds-DavidBouchardGagnon-v01b-210510-864_new-90ik 9090 1000 1200 90
+./custom-cli-start-script-docker-dev-specific-checkpoint-doubletwo.210531.sh model_gia-ds-DavidBouchardGagnon-v01b-210510-864_new-90ik 9090 1000 1200 90
