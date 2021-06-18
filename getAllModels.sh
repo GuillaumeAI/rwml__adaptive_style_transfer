106cf0971a2a47d1edec56d47fe660808f3a16a4
Ubuntu <ubuntu@ip-172-31-39-169.ca-central-1.compute.internal>
2021-06-18 01:51:47 +0000

    fix

--- getAllModels.sh
+++ getAllModels.sh
@@ -1,1 +1,1 @@
-(cdm &> /dev/null ; for m in $(ls -d model_*) ; do if [ -e "$m/checkpoint_long/checkpoint" ] ; then echo "$m";fi ; done)
+(cd /a/model/models &> /dev/null ; for m in $(ls -d model_*) ; do if [ -e "$m/checkpoint_long/checkpoint" ] ; then echo "$m";fi ; done)
