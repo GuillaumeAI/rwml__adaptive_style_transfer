3e48ba24a16db4135cd6d395a6d3a1aa1e253587
Ubuntu <ubuntu@ip-172-31-39-169.ca-central-1.compute.internal>
2021-06-27 10:41:04 +0000

    cleanup

--- custom-start-docker-ast-compo-pica_v03_60-doubletwo-dev-v1.sh
+++ custom-start-docker-ast-compo-pica_v03_60-doubletwo-dev-v1.sh
@@ -1,18 +1,11 @@
 source _env.sh
 
 # ARTIST : CHG Model name
-#export modelname="model_gia-ds-fpolsonwill_v02_210424_new-195ik"
-#export model2name="model_gia-young-picasso-v05-2101082232_new-150ik"
 
-#export modelname="model_gia-ds-DavidBouchardGagnon-v01b-210510-864_new"
-#model_gia-ds-DavidBouchardGagnon-v01b-210510-864_new-90ik_
+
+
 export modelname="model_gia-young-picasso-v03-201216_new-60ik"
-#export model2name="model_gia-ds-DavidBouchardGagnon-v01b-210510-864_new-270ik"
-#export model3name="model_gia-ds-DavidBouchardGagnon-v01b-210510-864_new-60ik"
-# The resolution of the two passes
-#export PASS1IMAGESIZE=1328
-#export PASS2IMAGESIZE=1024
-#export PASS3IMAGESIZE=2048
+
 export PASS1IMAGESIZE=700
 export PASS2IMAGESIZE=2000
 #export PASS3IMAGESIZE=1538
