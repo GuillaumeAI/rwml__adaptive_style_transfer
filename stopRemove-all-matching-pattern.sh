e3b13c2382ab9bd83c723c0670d17e895d4c3a0e
JGWill-Orko <jgi@jgwill.com>
2021-06-29 13:59:42 -0400

    fix

--- stopRemove-all-matching-pattern.sh
+++ stopRemove-all-matching-pattern.sh
@@ -7,9 +7,15 @@ if [ "$1" == "" ]; then
 	echo "$0 ast_ds-GabrielleKelly"
 	exit 1
 fi
+#if [ ! "$(dkcrm)" ] ; then . $HOME/.bash_aliases; fi
+#echo no; else echo ya ; fi
+#if [ ! "$(dkcrm)" ] ; then echo no; else echo ya ; fi
+#exit
 source $binroot/__fn.sh
+dkcrm=/a/bin/dkcRemove.sh
+
 #s="./_adm__stopRemove_Containers__210502.sh --list --port | sort"
 r=$(./_adm__stopRemove_Containers__210502.sh --list |grep "$1" | tr ":" " " | awk '// {print $1}')
 echo "Processing : $r"
 
-for l in $r; do echo "$l";dkcrm $l;done
+for l in $r; do echo "$l";$dkcrm $l;done
