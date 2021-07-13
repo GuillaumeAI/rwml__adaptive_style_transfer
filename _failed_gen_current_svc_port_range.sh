f13f756810996c0ffa61551a07eae67f4c2d3d91
Ubuntu <ubuntu@ip-172-31-39-169.ca-central-1.compute.internal>
2021-07-13 21:44:50 +0000

    fixed

--- _failed_gen_current_svc_port_range.sh
+++ _failed_gen_current_svc_port_range.sh
@@ -6,9 +6,14 @@ containers=$(cd /a/src/rwml__adaptive_style_transfer;./_adm__stopRemove_Containe
 
 for c in $containers ; do 
 	p=$(echo $c | tr ":" " " | awk '/90/ {print substr($1,3)}')
+	#echo "p=$p"
+	#echo "----"
 	n=$(echo $c | tr ":" " " | awk '/90/ {print $2}')
+	#echo "n=$n"
+	#echo "---"
 	#" #"$2}'  )
-	echo "$1" $p "$2" "#$n $3"
+	#echo "$1 $p $2 # $n $3"
+	echo "$1 $2 $p $3 # $n $4"
 
 done
 
