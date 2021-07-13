d48ee336fd1d73d3327fe616048e7c29df9c1fa8
Ubuntu <ubuntu@ip-172-31-39-169.ca-central-1.compute.internal>
2021-07-13 21:34:05 +0000

    workip

--- _failed_gen_current_svc_port_range.sh
+++ _failed_gen_current_svc_port_range.sh
@@ -1,3 +1,17 @@
-(cd /a/src/rwml__adaptive_style_transfer;./_adm__stopRemove_Containers__210502.sh --list --port  | sort|tr ":" " " | awk '/90/ {print substr($1,3)  " #"$2}'  )
+#(cd /a/src/rwml__adaptive_style_transfer;./_adm__stopRemove_Containers__210502.sh --list --port  | sort|tr ":" " " | awk '/90/ {print substr($1,3)  " #"$2}'  )
+
+containers=$(cd /a/src/rwml__adaptive_style_transfer;./_adm__stopRemove_Containers__210502.sh --list --port  | sort| awk '/90/')
+
+
+
+for c in $containers ; do 
+	p=$(echo $c | tr ":" " " | awk '/90/ {print substr($1,3)}')
+	n=$(echo $c | tr ":" " " | awk '/90/ {print $2}')
+	#" #"$2}'  )
+	echo "$1" $p "$2" "#$n $3"
+
+done
+
+
 
 
