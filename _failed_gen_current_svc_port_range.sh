fbe182c8c8f600009e9d389723ba9d285995b61e
Ubuntu <ubuntu@ip-172-31-39-169.ca-central-1.compute.internal>
2021-07-13 21:50:04 +0000

    add:usage-ie

--- _failed_gen_current_svc_port_range.sh
+++ _failed_gen_current_svc_port_range.sh
@@ -1,5 +1,11 @@
 #(cd /a/src/rwml__adaptive_style_transfer;./_adm__stopRemove_Containers__210502.sh --list --port  | sort|tr ":" " " | awk '/90/ {print substr($1,3)  " #"$2}'  )
 
+if [ "$1" == "" ] ; then
+	echo "Usage : $0 <cmd> <file> \"<args>\" \"<comments\">"
+	echo "ie.: . _failed_gen_current_svc_port_range.sh gia-ast-meta Sketch__2101240002__01_cc01-redraw.jpg \"380 2048 -1\""
+	exit 1
+fi
+
 containers=$(cd /a/src/rwml__adaptive_style_transfer;./_adm__stopRemove_Containers__210502.sh --list --port  | sort| awk '/90/')
 
 
