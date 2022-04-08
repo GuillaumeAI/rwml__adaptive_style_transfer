4f0fb7ade3204a971ff00c69c7ebe4aaa0910817
Jean Guillaume <jgi@jgwill.com>
2022-04-08 14:29:41 -0400

    upgraded

--- _Okni__start.sh
+++ _Okni__start.sh
@@ -1,36 +1,61 @@
 
+. $rwroot/_env.sh &>/dev/null || . _env.sh || echo "Could not load env"
 
 #@STCGoal Okni 2204 AICollaborator service management script
 #@STCStatus creating it....
+ifneecho() {
+	local _str="$1"
+	if [ "$_str" != "" ]; then echo "$_str";fi
+}
+
+ifnotemptyecho() {
+	ifneecho "$1"
+}
+
 
 containername="ast_dbginko-v03-210809-864x_300m_s1"
-containerstarter() {
-	local _containername="$1"
-	local _installcmd="$2"
-	local _optionalmsgsuccess="$3"
-	local _optionalmsgfailed="$4"
-	local _tst=$(docker ps -a --filter "name=$_containername" | awk '/'"$_containername"'/')
-	if [ "$_tst" == "" ]; then
-		echo "$_containername not installed"
-			        $_installcmd &> /dev/null && echo "$_containername Installed" || echo "Failed to install $_containername"
+containerstarterinstaller() {
+	local _action="$1"
+	local _containername="$2"
+	local _installcmd="$3"
+	local _title="$4"
+	local _usage="Usage: containerstarterinstaller <start|stop|remove> <containername> [installcmd] [title]"
+	#if [ "$_action" == "" ] ; then echo "$_usage"; return;fi
+	#check if first argument is an action
+	if ! [[ "$_action" =~ ^(start|stop|remove)$ ]]; then echo "$_usage"; return;fi
+
+	#if [ "$_title" != "" ]; then echo "$_title"; fi
+	ifneecho "$_title"
+
+	#local _optionalmsgsuccess="$3"
+	#local _optionalmsgfailed="$4"
+	if [ "$_action" == "start" ]; then
+
+		local _tst=$(docker ps -a --filter "name=$_containername" | awk '/'"$_containername"'/')
+	
+		if [ "$_tst" == "" ]; then
+			echo "$_containername not installed"
+			$_installcmd &> /dev/null && echo "$_containername Installed" || echo "Failed to install $_containername"
+		else
+			echo "$_containername Sounds great" 
+		
+			#make sure its started
+			docker start $_containername &> /dev/null  #...and dont botther with errors
+		fi
 	else
-		echo "$_containername Sounds great" 
-		#make sure its started
-		docker start $_containername &> /dev/null  #...and dont botther with errors
+		if [ "$_action" == "stop" ]; then 
+			docker stop $_containername &> /dev/null && echo "Stopped $_containername"
+		fi
 	fi
 }
 
 #meta server dependencies
 metacontainername="ast_meta_server"
-tst=$(docker ps -a --filter "name=$metacontainername" | awk '/'"$metacontainername"'/')
-if [ "$tst" == "" ]; then 
-	echo "not installed"
-	. install-httpd-meta-container.sh &> /dev/null && echo "AST Meta Server Installed"
-else 
-	echo "AST Meta Server Sounds great" 
-	docker start ast_meta_server &> /dev/null #|| (echo "ohoh, sounds like AST Meta Server not installed, installing :)" && . install-httpd-meta-container.sh &> /dev/null && echo "AST Meta Server Installed")
-fi
+#containerstarterinstaller
+#sleep 3
+containerstarterinstaller start "$metacontainername" ". install-httpd-meta-container.sh" "AST Meta Server"
 
+containerstarterinstaller start "$containername" "echo TODO cmd installer" "Okni 9060 300ki"
 #our 
 
-docker start $containername && echo "Started $containername"
+#docker start $containername && echo "Started $containername"
