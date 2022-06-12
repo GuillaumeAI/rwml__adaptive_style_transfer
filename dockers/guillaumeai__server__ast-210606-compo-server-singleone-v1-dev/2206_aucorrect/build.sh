ad51c4c539897015bcd655d55bf8812b783bd2c8
Jean Guillaume <jgi@jgwill.com>
2022-06-12 02:57:51 -0400

    fix:OpenCV-python27

--- dockers/guillaumeai__server__ast-210606-compo-server-singleone-v1-dev/2206_aucorrect/build.sh
+++ dockers/guillaumeai__server__ast-210606-compo-server-singleone-v1-dev/2206_aucorrect/build.sh
@@ -1,11 +1,13 @@
 containertag=guillaumeai/server:ast-210606-singleone-v1-dev-acc
-copy -f ../../../compo-singleone-v1-dev-acc.py . && \
-docker build -t $containertag . --no-cache
+cp -f ../../../compo-singleone-v1-dev-acc.py . && \
+docker build -t $containertag . $1
+#--no-cache
 #docker build -t $containertag . 
 sleep 1
 
 if [ "$1" == "" ]; then
-docker push $containertag
+echo "Pushing"
+#docker push $containertag
 fi
 
 
