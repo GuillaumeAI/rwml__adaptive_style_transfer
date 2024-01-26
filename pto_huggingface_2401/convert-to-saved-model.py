f069f8734161851c9cebdbe8bdf45ae05dabc82a
JGWill <jgi@jgwill.com>
2024-01-26 18:26:56 -0500

    pto:Load converted model

---
+++ pto_huggingface_2401/convert-to-saved-model.py
@@ -0,0 +1,21 @@
+import os
+import tensorflow as tf
+model_root='/var/lib/ast/model'
+
+trained_checkpoint_relpath="model_gia-young-picasso-v02b-201210-864_new-240ik/checkpoint_long/model_gia-young-picasso-v02b-201210-864_new_240000.ckpt-240000"
+trained_checkpoint_prefix = model_root +'/'+trained_checkpoint_relpath
+export_dir="/var/lib/ast/export/model_gia-young-picasso-v02b-201210-864_new-240ik/export2401261700"
+#export_dir = os.path.join('/a/model/models/model_gia-ds-DavidBouchardGagnon-v02-210702-864x_new-240ik-savebd', '0')
+
+graph = tf.Graph()
+with tf.compat.v1.Session(graph=graph) as sess:
+    # Restore from checkpoint
+    loader = tf.compat.v1.train.import_meta_graph(trained_checkpoint_prefix + '.meta')
+    loader.restore(sess, trained_checkpoint_prefix)
+
+    # Export checkpoint to SavedModel
+    builder = tf.compat.v1.saved_model.builder.SavedModelBuilder(export_dir)
+    builder.add_meta_graph_and_variables(sess,
+                                         [tf.saved_model.TRAINING, tf.saved_model.SERVING],
+                                         strip_default_attrs=True)
+    builder.save()     
