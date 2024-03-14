a89ebb65411841356573c95dd5efbb8369d87f39
JGWill <jgi@jgwill.com>
2024-03-14 18:29:12 -0400

    working

---
+++ __launch-docker-dev-specific-checkpoint-singleone.210606-nvidia.sh
@@ -0,0 +1,183 @@
+#!/bin/bash
+
+#########################################################
+# Model Web API Checkpoint Server DoubleTwo             #
+# This version should receive a checkpoint number       #
+# as input to mount along with the modelname            #
+# By Guillaume Descoteaux-Isabelle, 2021 (UQAC/NAD)     #
+#########################################################
+
+# 
+if [ "$1" == "--fg" ]; then 
+	docker_mode="it"
+	docker_run_args="--rm"
+fi
+export docker_cmd="docker run -$docker_mode $docker_run_args --name $containername "
+if [ $docker_mode = "d" ] ; then echo "Background infrastructure mode activated (will run in background until stopped or server rebooted)" ; fi
+if [ $docker_mode = "it" ] ; then echo "Foreground infrastructure mode activated (require to keep the startup shell active)" ; fi
+
+
+export callurl="$callprotocol://$hostdns:$serverhostport/stylize"
+# local path and container mount path : modelmountpoint modellocalpoint
+export modelmountpoint="$containermodelroot/$modelname/checkpoint_long"
+export modellocalpoint="$modelmountpath/$modelname/checkpoint_long"
+
+####@TODO MAke the 3 model file and checkpoint specific file
+# $modelmountpath  = /a/model/models/$modelname/checkpoint_long
+# $modellocalpoint # The root where we will mount the files
+# Files :
+# .ckpt-195000.index .ckpt-195000.meta .data-00000-of-00001 model_gia-ds-fpolsonwill_v02_210424_new_195000. checkpoint
+# 3 files
+checkpointpad=$checkpointno'000'
+mindex='.ckpt-'$checkpointpad'.index'
+mmeta='.ckpt-'$checkpointpad'.meta'
+#.ckpt-195000.data-00000-of-00001
+mdata='.ckpt-'$checkpointpad'.data-00000-of-00001'
+mfilesuffix='.ckpt-'$checkpointpad
+mfilepresuffix='_'$checkpointpad
+
+# Content of the ./checkpoint file
+#>>model_checkpoint_path: "model_gia-ds-fpolsonwill_v02_210424_new_195000.ckpt-195000"
+#>>all_model_checkpoint_paths: "model_gia-ds-fpolsonwill_v02_210424_new_195000.ckpt-195000"
+tmpmodelfilename=$(cd $modellocalpoint;ls *$mmeta)
+replstring='_'$checkpointpad$mmeta
+secondString=""
+tmpbasename="${tmpmodelfilename/$replstring/$secondString}"
+
+checkpointbasefilename=$tmpbasename
+# mcheckpointfilecontentline1='model_checkpoint_path: "'$checkpointbasefilename$mfilepresuffix$mfilesuffix'"'
+# mcheckpointfilecontentline2='all_model_checkpoint_paths: "'$checkpointbasefilename$mfilepresuffix$mfilesuffix'"'
+astia_server_file_location='/tmp/astia'
+mkdir -p $astia_server_file_location
+mcheckpointfilepath=$astia_server_file_location'/'$modelname'_checkpoint_'$checkpointno
+mindexfile=$checkpointbasefilename$mfilepresuffix$mindex
+mmetafile=$checkpointbasefilename$mfilepresuffix$mmeta
+mdatafile=$checkpointbasefilename$mfilepresuffix$mdata
+
+testing=0
+if [ "$testing" == "1" ]; then
+
+	echo "-----------------------------------------------Tests: "
+	#echo "- tmpmodelfilename=$tmpmodelfilename"
+	#echo "- replstring=$replstring"
+	#echo "- checkpointbasefilename=$checkpointbasefilename"
+	#echo "- mcheckpointfilecontent=$mcheckpointfilecontent"
+	#echo "- remove : $replstring in $tmpmodelfilename and use to construct the checkpoint file"
+	#echo "-- it gives: $tmpbasename"
+
+	echo "<<"
+	#echo "- checkpointbasefilename=$checkpointbasefilename"
+	#echo "- mfilepresuffix=$mfilepresuffix"
+	#echo "- mfilesuffix=$mfilesuffix"
+	echo "- mindexfile=$mindexfile"
+	echo "- mmetafile=$mmetafile"
+	echo "- mdatafile=$mdatafile"
+fi
+# Making the local checkpoint we will mount
+#echo "$mcheckpointfilecontentline1" > $mcheckpointfilepath
+#echo "$mcheckpointfilecontentline2" >> $mcheckpointfilepath
+makecheckpointfile $modelname $checkpointno
+export mcheckpointfilepath="$MCHECKPOINTFILEPATH"
+
+# Making path to mount from local to the docker host foreach of the 3 files
+##modelmountpoint modellocalpoint
+export modelmountpointmeta=$modelmountpoint/$mmetafile
+export modelmountpointindex=$modelmountpoint/$mindexfile
+export modelmountpointdata=$modelmountpoint/$mdatafile
+export modelmountpointckfile=$modelmountpoint/checkpoint
+export modellocalpointmeta=$modellocalpoint/$mmetafile
+export modellocalpointindex=$modellocalpoint/$mindexfile
+export modellocalpointdata=$modellocalpoint/$mdatafile
+
+#echo "Exting because we are testing" ;exit 1
+
+#############
+echo "-------------------------------------------------------------"
+echo "- Running  Guillaume's M.A. AI Model Server SingleOne     -"
+echo "- Model: $modelname"
+echo "- Model Checkpoint: $checkpointno"
+echo "- AccessURL : $callurl"
+echo "-------------------------------------------------------------"
+
+#echo "Exting because we are testing" ;exit 1
+
+#$docker_cmd -v $(pwd):/work -p 8000:9000 -v $modellocalpoint:$modelmountpoint -p $serverhostport:$serverport $containertag $run_cmd
+echo "----------Cleaning up $containername-------"
+docker stop $containername  &> /dev/null
+docker rm $containername  &> /dev/null && echo "--Cleanup done" || echo "-- nothing to cleanup" 
+echo "-----------Installing $containername ------------"
+
+
+#echo "Exting because we are testing" ;exit 1
+export AUTOABC_=""
+if [ "$autoabc" != "" ]; then export AUTOABC_="-e AUTOABC=$autoabc"
+	else
+	echo "AUTOABC IS OFF OR NOT Defined (export autoabc=1)"
+fi
+
+nvidia_arg=" --runtime=nvidia --gpus all "
+
+execme="$docker_cmd \
+	$nvidia_arg \
+	-v $(pwd):/work  $AUTOABC_ \
+	-v /a/bin:/a/bin \
+	 -e PASS1IMAGESIZE=$PASS1IMAGESIZE \
+	 -e MODELNAME=$modelname \
+	 -e MODEL1NAME=$modelname  \
+	-v $modellocalpointmeta:$modelmountpointmeta \
+	-v $modellocalpointindex:$modelmountpointindex \
+	-v $modellocalpointdata:$modelmountpointdata \
+	-v $mcheckpointfilepath:$modelmountpointckfile \
+	-p $serverhostport:$serverport \
+	 -e SPORT=$serverhostport $singleonev1devcontainertag"
+
+#@a Save model metadata for further id of results
+#@state We store by Port
+
+metafilename=$serverhostport.json
+mkdir -p $metahttpdocastinfopath
+metafile=$metahttpdocastinfopath/$metafilename
+if [ "$hostdns" == "" ] ; then export hostdns=localhost;fi
+getmetaurl="$callprotocol://$hostdns/$metafilename"
+
+fname="$(getfnamefrommodel $modelname)$autoabc"
+echo "fname=$fname"
+echo "--------------------------"
+sleep 1
+echo "{ " >   $metafile
+echo "\"modelname\":\"$modelname\"," >>  $metafile
+echo "\"fname\":\"$fname\"," >>  $metafile
+echo "\"containername\":\"$containername\",">>    $metafile
+echo "\"containertag\":\"$compo3v2devcontainertag\",">>    $metafile
+echo "\"checkpointno\":\"$checkpointno\",">>    $metafile
+echo "\"svrtype\":\"s1\",">>    $metafile
+echo "\"mtype\":\"ast\",">>    $metafile
+echo "\"type\":\"singleone\",">>    $metafile
+echo "\"autoabc\":\"$autoabc\",">>  $metafile
+echo "\"callurl\":\"$callurl\",">>    $metafile
+echo "\"PASS1IMAGESIZE\":\"$PASS1IMAGESIZE\"," >>    $metafile 
+echo "\"getmetaurl\":\"$getmetaurl\"," >>    $metafile
+echo "\"created\":\"$(date)\"" >>    $metafile
+echo "}">>    $metafile
+
+#@STCGoal Central registration of currently running services
+if [ "$metaglobalregistryfeature" == "1" ] ; then
+	        #moved _env.sh export globallocationpath=/home/jgi/astiapreviz
+	        cdir=$(pwd)
+	        gtpath=$globallocationpath/svr/$HOSTNAME
+	        mkdir -p $gtpath
+	        (cp $metafile $gtpath && cd $gtpath && ls *json> list.txt)  &
+fi
+
+echo "$execme"
+sleep 1
+#echo "$serverhostport"
+
+$execme
+
+
+if [ "$statusafterlaunch" == "stop" ]; then 
+	echo "variable : \$statusafterlaunch == stop"
+	docker stop $containername  &> /dev/null  && echo "$containername Stopped"
+fi
+#sleep 1
---
+++ custom-cli-start-script-docker-dev-specific-checkpoint-singleone.210606-nvidia.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Dev to mount specific checkpoint and launch an API Web service serving those checkpoints.
+source $binroot/__fn.sh
+
+# script.sh MODEL_NAME APIPORT IMGSIZE1 CHECKPOINT_NUM [--fg(debug)] 
+
+source _env.sh
+
+showhelp=0
+if [ "$1" == "" ]; then
+	showhelp=1
+fi
+if [ "$2" == "" ]; then
+        showhelp=1
+fi
+if [ "$3" == "" ]; then
+        showhelp=1
+fi
+if [ "$4" == "" ]; then
+        showhelp=1
+fi
+
+if [ "$showhelp" == "1" ]; then
+	echo "Bad arguments"
+	echo "Usage:"
+	echo "	$0 MODELNAME APIPORT PASS1IMAGESIZE CHECKPOINT_NUM [--fg (debug)] "
+	echo "	# CHECKPOINT_NUM = 15 30 45 60 75 90 105 120 135 150 165 180 195 210 225 240 255 270 285 300 315 330 345 360 375 390 405"
+        echo "        # API PORT = 01-99 		"
+	exit 1
+fi
+
+
+
+export modelname="$1"
+export serverhostport=$2
+export PASS1IMAGESIZE=$3
+
+export checkpointno="$4"
+
+# Take care of cleaning the model name to fabric a pretty name for the container
+replacerstr="model_gia-ds-"
+secondString=""
+modelnametmp=$modelname
+tmpname="${modelnametmp/$replacerstr/$secondString}"
+replacerstr="model_gia"
+modelnametmp=$tmpname
+tmpname="${modelnametmp/$replacerstr/$secondString}"
+replacerstr="model_"
+modelnametmp=$tmpname
+tmpname="${modelnametmp/$replacerstr/$secondString}"
+replacerstr="_new"
+modelnametmp=$tmpname
+tmpname="${modelnametmp/$replacerstr/$secondString}"
+export containername='ast_'$tmpname'_'$checkpointno'm_s1_'$autoabc
+
+
+
+echo "source __launch-docker-dev-specific-checkpoint-singleone.210606.sh $5 $6"
+source __launch-docker-dev-specific-checkpoint-singleone.210606-nvidia.sh $5 $6
+
+
