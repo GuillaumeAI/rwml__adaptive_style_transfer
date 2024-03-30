d6f9b7f959eafb079fc59712ac550d8e868ae6e8
JGWill <jgi@jgwill.com>
2024-03-30 07:21:28 -0400

    proto

---
+++ dockers/ast-base-2403/_env.sh
@@ -0,0 +1,61 @@
+ctlid=2403300347
+dockertag=guillaumeai/ast:base-2403
+
+containername=ast-base-2403
+dkhostname=$containername
+
+# PORT
+dkport=9861:7860
+
+model_foldername=model_cezanne
+#model_foldername=model_gauguin
+xmount=/var/lib/ast/model/$model_foldername:/data/styleCheckpoint/$model_foldername
+xmount2=/a/src/ast:/ast
+
+#dkcommand=bash #command to execute (default is the one in the dockerfile)
+
+#dkextra=" -v \$dworoot/x:/x -p 2288:2288 "
+
+#dkmounthome=true
+
+
+##########################
+############# RUN MODE
+#dkrunmode="bg" #default fg
+#dkrestart="--restart" #default
+#dkrestarttype="unless-stopped" #default
+
+
+#########################################
+################## VOLUMES
+#dkvolume="myvolname220413:/app" #create or use existing one
+#dkvolume="$containername:/app" #create with containername name
+
+
+
+#dkecho=true #just echo the docker run
+
+
+# Use TZ
+#DK_TZ=1
+
+
+
+#####################################
+#Build related
+#
+##chg back to that user
+#dkchguser=vscode
+
+######################## HOOKS BASH
+### IF THEY EXIST, THEY are Executed, you can change their names
+
+dkbuildprebuildscript=dkbuildprebuildscript.sh
+dkbuildbuildsuccessscript=dkbuildbuildsuccessscript.sh
+dkbuildfailedscript=dkbuildfailedscript.sh
+dkbuildpostbuildscript=dkbuildpostbuildscript.sh
+
+###########################################
+# Unset deprecated
+unset DOCKER_BUILDKIT
+
---
+++ dockers/ast-base-2403/dkbuildprebuildscript.sh
@@ -0,0 +1,24 @@
+cp /a/src/ast/v2/compo-singleone-v2-dev-acc.py .
+cp /a/src/ast/compo-singleone-v1-dev-acc.py .
+
+
+
+cp ../../compo-server-doubletwo-v1-dev.py .
+cp ../../compo-server-doubletwo-v2-dev-png.py .
+cp ../../compo-server.py .
+cp ../../compo-server-three.py .
+cp ../../compo-server-three-v2-dev.py .
+cp ../../compo-server-two-v2-dev.py .
+cp ../../compo-singleone-v1-dev.py .
+cp ../../convert-to-saved-model.py .
+cp ../../server.py .
+
+
+
+
+
+cp -r ../../runner .
+
+echo "dkbuildprebuildscript.sh executed"
+sleep 1
+
