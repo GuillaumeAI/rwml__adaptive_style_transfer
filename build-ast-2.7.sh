d01b0e4014785ab815ef5f92bff7e3513f77c7f1
JGWill <jgi@jgwill.com>
2024-03-30 05:59:55 -0400

    build notes for the ast 2.7

---
+++ build-ast-2.7.sh
@@ -0,0 +1,11 @@
+# build notes for the ast 2.7
+
+cdrw
+newcon ast " " 2.7 
+
+pip install opencv-python==4.2.0.32 cryptography==2.8 
+pip install tqdm 
+pip install requests 
+
+sudo apt install -y libre2-dev 
+pip install re2 
