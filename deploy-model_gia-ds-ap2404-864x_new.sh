806812a2192e438815136223c42df2535153673c
JGWill <jgi@jgwill.com>
2024-04-10 08:56:31 -0400

    enhanced: Launch only if not running
    * --force option

--- deploy-model_gia-ds-ap2404-864x_new.sh
+++ deploy-model_gia-ds-ap2404-864x_new.sh
@@ -26,7 +26,7 @@ for c in $chks ; do
 	#export checkpointno=345
 	# @mkcontainername $m 
 	export containername=$(mkcontainername $m $checkpointno)
-	echo "Containername: $containername"
+	#echo "Containername: $containername"
 	#sleep 12
 	#export containername='pikawill03a2-'$c'uk'
 
@@ -34,19 +34,30 @@ for c in $chks ; do
 		echo -n "Removing container $containername.."
 		dkcrm $containername  &>/dev/null && echo "..removed"
 	else
-		
+		#echo "Checking $containername is running : curl http://localhost:$serverhostport/stylize"
+		do_it=$(curl "http://localhost:$serverhostport/stylize" &>/dev/null && echo "no"||echo "yes")			
+		if [ "$1" == "--force" ]; then
+			do_it="yes"
+			echo "Forcing launch"
+			sleep 1
+		fi
+
 		export docker_mode="d"
 
 		export docker_run_args="--restart unless-stopped "
 
-
-		source custom-cli-start-script-docker-dev-specific-checkpoint-singleone.210606.sh \
-			$modelname \
-			$serverhostport \
-			$PASS1IMAGESIZE \
-       			$checkpointno
-	
+		if [ "$do_it" == "yes" ]; then
+			
+			source custom-cli-start-script-docker-dev-specific-checkpoint-singleone.210606.sh \
+				$modelname \
+				$serverhostport \
+				$PASS1IMAGESIZE \
+       				$checkpointno
+		else 
+			echo "$containername already running, Skipping $serverhostport"
+		fi
 		serverhostport=$(expr $serverhostport + 1)
+		
 	fi
 
 done
